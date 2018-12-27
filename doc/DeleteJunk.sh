f8d17d99674c8d67a1bd3e9d708718655068ba84
Carlos Manuel <fis.carlosmanuel@gmail.com>
2018-12-27 14:30:43 -0600

    Finalización de reporte

---
+++ doc/DeleteJunk.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+rm -f *.synctex.gz
+rm -f *.aux
+rm -f *.log
+rm -f *.fdb_latexmk
+rm -f *.spl
+rm -f *.fls
+rm -f *.blg
+rm -f *.out
+rm -f *.bbl
+rm -f *.idx
+rm -f *.bak
+rm -f *.toc
